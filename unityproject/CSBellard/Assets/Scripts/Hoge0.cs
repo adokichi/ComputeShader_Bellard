28209c5c69002ae506dc5afb637c906e8867c5f7
toropippi <44022497+toropippi@users.noreply.github.com>
2020-05-25 10:55:11 +0900

    debug0

--- unityproject/CSBellard/Assets/Scripts/Hoge0.cs
+++ unityproject/CSBellard/Assets/Scripts/Hoge0.cs
@@ -6,31 +6,37 @@ using UnityEngine;
 public class Hoge0 : MonoBehaviour
 {
     public ComputeShader shader;
+    float[] host_A;
+    float[] host_B;
+    float[] host_C;
+
+    ComputeBuffer A;
+    ComputeBuffer B;
+    ComputeBuffer C;
+    ComputeBuffer bigSum;
+    int k, k2,cnt;
+    ulong d, offset, k_max, k_max_end;
+    int[] inputint2;
+    int flag;
 
     void ulongtouint2(ulong a,int[] b) {
         b[0] = (int)(a % (ulong)4294967296);
         b[1] = (int)(a / (ulong)4294967296);
-        //Debug.Log(a);
-        //Debug.Log(b[0]);
-        //Debug.Log(b[1]);
     }
 
     void Start()
     {
+        host_A = new float[] { 1f, 1f, 1f, 1f };
+        host_B = new float[] { 1f, 1f, 1f, 1f };
+        host_C = new float[] { 0f, 0f, 0f, 0f };
 
-        float[] host_A = { 1f, 1f, 1f, 1f };
-        float[] host_B = { 1f, 1f, 1f, 1f };
-        float[] host_C = { 0f, 0f, 0f, 0f };
-
-        ComputeBuffer A = new ComputeBuffer(host_A.Length, sizeof(float));
-        ComputeBuffer B = new ComputeBuffer(host_B.Length, sizeof(float));
-        ComputeBuffer C = new ComputeBuffer(host_C.Length, sizeof(float));
-
-        ComputeBuffer bigSum = new ComputeBuffer(4096 * 256 * 3, sizeof(ulong));
-        //ComputeBuffer dbg0 = new ComputeBuffer(1, sizeof(ulong));
-
-        int k = shader.FindKernel("CSMain");
-        int k2 = shader.FindKernel("Sglobal64mtg_192_Refresh");
+        A = new ComputeBuffer(host_A.Length, sizeof(float));
+        B = new ComputeBuffer(host_B.Length, sizeof(float));
+        C = new ComputeBuffer(host_C.Length, sizeof(float));
+        bigSum = new ComputeBuffer(4096 * 256 * 3, sizeof(ulong));
+        
+        k = shader.FindKernel("CSMain");
+        k2 = shader.FindKernel("Sglobal64mtg_192_Refresh");
         
         // host to device
         A.SetData(host_A);
@@ -43,23 +49,13 @@ public class Hoge0 : MonoBehaviour
         shader.SetBuffer(k2, "bigSum", bigSum);
         //shader.SetBuffer(k2, "dbg0", dbg0);
 
-
-        /*
-    uint2 offset;
-    uint2 d;
-    uint2 k_max;
-    uint numesign;
-    uint den0;
-    uint den1;
-    */
-
-        ulong d = 799999999999999;
-        ulong offset =    137438953472000;
-        ulong k_max = offset;
-        ulong k_max_end = 137438993472000;
+        d = 799999999999999;
+        offset =    137438953472000;
+        k_max = offset;
+        k_max_end = 137438993472000;
         //ulong k_max_end = 137576392425472;
 
-        int[] inputint2=new int[2];
+        inputint2=new int[2];
         ulongtouint2(d, inputint2);
         
         shader.SetInts("d", inputint2);
@@ -67,23 +63,40 @@ public class Hoge0 : MonoBehaviour
         shader.SetInt("den0", 4);
         shader.SetInt("den1", 1);
 
-        
-        for (;; )
+        cnt = 0;
+        flag = 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag == 1) 
         {
             offset = k_max;
             k_max = k_max + 256 * 4096 * 8;
             if (k_max > k_max_end) k_max = k_max_end;
+
             ulongtouint2(offset, inputint2);
             shader.SetInts("offset", inputint2);
             ulongtouint2(k_max, inputint2);
             shader.SetInts("k_max", inputint2);
+
             // GPUで計算
             shader.Dispatch(k2, 4096, 1, 1);
             C.GetData(host_C);
-            if (k_max == k_max_end) break;
+            Debug.Log(cnt);
+            if (k_max == k_max_end)
+            {
+                flag = 0;
+                endf();
+            }
         }
 
+        cnt++;
+    }
 
+    void endf() {
 
         // device to host
         C.GetData(host_C);
@@ -93,13 +106,15 @@ public class Hoge0 : MonoBehaviour
         ulong[] ulres = new ulong[4096 * 256 * 3];
         bigSum.GetData(ulres);
         ulong ans0 = 0, ans1 = 0, ans2 = 0;
-        for (int i= 0; i < 4096 * 256;i++) {
+        for (int i = 0; i < 4096 * 256; i++)
+        {
             ans0 += ulres[i * 3 + 0];
-            if (ans0 < ulres[i * 3 + 0]) {
+            if (ans0 < ulres[i * 3 + 0])
+            {
                 ans1++;
                 if (ans1 == 0) ans2++;
             }
-            ans1+= ulres[i * 3 + 1];
+            ans1 += ulres[i * 3 + 1];
             if (ans1 < ulres[i * 3 + 1])
             {
                 ans2++;
@@ -129,13 +144,6 @@ public class Hoge0 : MonoBehaviour
         B.Release();
         C.Release();
         bigSum.Release();
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
 
