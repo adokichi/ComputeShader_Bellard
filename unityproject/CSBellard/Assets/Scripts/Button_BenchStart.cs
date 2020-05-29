2ff2634e6aa306303560328fb0a5f10c8b3fc84a
toropippi <44022497+toropippi@users.noreply.github.com>
2020-05-30 03:48:30 +0900

    inore修正

---
+++ unityproject/CSBellard/Assets/Scripts/Button_BenchStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Button_BenchStart : MonoBehaviour
+{
+    public void OnClick()
+    {
+
+        //HttpService h=GameObject.Find("HttpService").GetComponent<HttpService>();
+        //StartCoroutine(HttpService.Post());
+        
+        string digits = GameObject.Find("TextDigits").GetComponent<Text>().text;
+        GetComponent<Hoge0>().ButtonPush(ulong.Parse(digits));
+        
+
+    }
+}
---
+++ unityproject/CSBellard/Assets/Scripts/FPSTextUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSTextUI : MonoBehaviour
+{
+    private Text text;
+    int cnt;
+    float ftime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+        Debug.Log(SystemInfo.graphicsDeviceName);
+        cnt = 0;
+        ftime = Time.time - 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - ftime >= 1.0f) {
+            ftime = Time.time;
+            string fpstxt;
+            fpstxt = "" + (cnt) + "";
+            text.text = "GPU name:" + SystemInfo.graphicsDeviceName + "\t\tFPS:" + fpstxt;
+            cnt = 0;
+        }
+        cnt++;
+    }
+}
